7ac65e6dc36ba8fa216d0111268e9e639ac60748
prajvalkhadse <prajvalkhadse2017>
2023-06-22 14:18:42 +0530

    First

---
+++ src/enviroments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/landon',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '9ef2c7c06amsh75482aea3e3a64ap1dac25jsn63da8a6d32ef'
+
+
+
+  };
+  
